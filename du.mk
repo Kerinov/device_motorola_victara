90f88c1d39b2a083b98593db7aacd994068c0ab2
Gabriel Freire <gabrielfvale@gmail.com>
2016-10-31 14:40:57 -0300

    Dirty af

---
+++ du.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/motorola/victara/full_victara.mk)
+
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_RELEASE_NAME := MOTO X (2014)
+PRODUCT_NAME := du_victara
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := XT1575
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
